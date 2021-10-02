68ede5f2d8d400bf2375b20d68b48b1bf97d4403
Babu Thomas <babuthomas138@gmail.com>
2021-10-02 20:33:56 +0530

    Add questions in ExcelColumnNumber

---
+++ ExcelColumnNumber/Main.java
@@ -0,0 +1,33 @@
+/*
+Q1. Given a decimal number, print it's octal representation.
+Sample Input => Output:
+    10 => 12
+    12 => 14
+    51 => 63
+    
+Q2. Given a decimal number, print it's hexadecimal representation.
+Sample Input => Output:
+    10 => A
+    12 => C
+    51 => 33
+
+Q3. Given an excel column name, print the column number.
+Sample Input => Output:
+    A => 1
+    AB => 28
+    BA => 53
+    XY => 649
+    
+Q4. Given an excel column number, print it's column name.
+Sample Input => Output:
+    1 => A
+    28 => AB
+    53 => BA
+    649 => XY
+*/
+
+class Main {
+  public static void main(String[] args) {
+    
+  }
+}
