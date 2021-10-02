dbc3a923d7c41bdf2610dbcc3eda99ad728a05fc
Babu Thomas <babuthomas138@gmail.com>
2021-10-02 19:54:39 +0530

    Add questions in DigitsOfNumber

---
+++ DigitsOfNumber/Main.java
@@ -0,0 +1,21 @@
+/*
+Q1. Multiplication of all digits in a number
+100 -> 0
+234 - > 24
+
+2. Find the magic number for a given number.
+Magic number is defined as:
+234 -> 24 -> 8
+0 -> 0
+4 -> 4
+19 -> 9
+ie. magic number is the multiplication of all digits of the number, then the multiplication of all digits in this result and so on
+till we get single digit number.
+*/
+
+class Main {
+  public static void main(String[] args) {
+    
+  }
+}
+
